0c5ac47e65c95d05213c929c683c6f2a37b16011
Kyo Jin <137484097+9817kg@users.noreply.github.com>
2023-11-30 18:32:04 +0900

    Add files via upload

---
+++ secondProject/src/main/java/service/RegisterMemberService.java
@@ -0,0 +1,39 @@
+package second.project.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import second.project.entity.Member;
+import second.project.repository.MemberRepository;
+
+
+
+@Service
+public class RegisterMemberService {
+	private final PasswordEncoder passwordEncoder;
+	private final MemberRepository repository;
+
+	@Autowired
+	public RegisterMemberService(PasswordEncoder passwordEncoder, MemberRepository repository) {
+		this.passwordEncoder = passwordEncoder;
+		this.repository = repository;
+	}
+
+	public Long join(String email, String password, String name, String role, String provider
+
+	) {
+
+		Member member = Member.createMember(email, password, name,passwordEncoder);
+		validateDuplicateMember(member);
+		repository.save(member);
+
+		return member.getId();
+	}
+
+	private void validateDuplicateMember(Member member) {
+		repository.findByEmail(member.getEmail()).ifPresent(m -> {
+			throw new IllegalStateException("이미 존재하는 회원입니다.");
+		});
+	}
+}
